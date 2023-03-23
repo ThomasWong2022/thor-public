eef12e620a866aea63aab6264cf8c0f9cf6c7919
ThomasWong2022 <mw4315@ic.ac.uk>
2023-03-23 08:45:12 +0000

    Update Setup

---
+++ buildpypi.sh
@@ -0,0 +1,6 @@
+## Standard Workflow
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade build
+python3 -m build
+python3 -m pip install --upgrade twine
+python3 -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="thor_public",
+    name="thorml",
     author="Thomas Wong",
     author_email="mw4315@ic.ac.uk",
     description="AutoML tools for Tabular Datasets",
